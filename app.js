4108ba48526c752563162c923172e86270835a3f
MahmoudFredj <mahmoudrayfredj@gmail.com>
2021-03-04 16:00:34 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const accessControl = require('./middlewares/accessControl.js');
+const { devDB, liveDB } = require('./config.json');
+
+app.use(accessControl);
+app.use(express.json({ limit: '64mb' }));
+
+
+// routers
+
+
+
+
+// connection Mongoose
+
+mongoose
+    .connect(devDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(
+        () =>
+            console.log(
+                '\x1b[32m',
+                `[mongoose] db connected on ${dbUrl} ...`,
+                '\x1b[37m',
+            ),
+        mongoose.set('useFindAndModify', false),
+    )
+    .catch((err) =>
+        console.log(
+            '\x1b[31m',
+            '[mongoose] db connection error :',
+            err,
+            '\x1b[37m',
+        ),
+    )
+
+const PORT = process.env.PORT || 2424;
+app.listen(PORT, () => {
+    console.log(
+        '\x1b[33m',
+        `[app] server running on port ${PORT} ...`,
+        '\x1b[37m',
+    )
+})
